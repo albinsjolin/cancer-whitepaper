5f47b823f90f2bbae617158814c73e7a0fa7fdc3
Albin Sjölin <albinsjoelin1@gmail.com>
2022-06-19 16:30:07 +0200

    feat: add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,13 @@ import { appWithTranslation } from 'next-i18next'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_TRACKING_ID}`} />
+      <Script>{`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag("js", new Date());gtag("config", "${process.env.NEXT_PUBLIC_TRACKING_ID}");`}</Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default appWithTranslation(MyApp)
