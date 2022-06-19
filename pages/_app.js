3ebd0aec8e2a7509b72d9088ada2ba44eba4dd7e
Albin Sjölin <albinsjoelin1@gmail.com>
2022-06-19 16:51:39 +0200

    fix: script id

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_TRACKING_ID}`} />
-      <Script>{`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag("js", new Date());gtag("config", "${process.env.NEXT_PUBLIC_TRACKING_ID}");`}</Script>
+      <Script id="my-script">{`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag("js", new Date());gtag("config", "${process.env.NEXT_PUBLIC_TRACKING_ID}");`}</Script>
       <Component {...pageProps} />
     </>
   )
