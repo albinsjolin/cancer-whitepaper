c148bb5265040c5cd6474d2362653fb98e07b1ce
Albin Sjölin <albinsjoelin1@gmail.com>
2022-06-28 21:06:40 +0200

    feat: add next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://www.cancer.com.ai',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+}
+
+module.exports = config
